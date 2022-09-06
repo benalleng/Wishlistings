1569d67b4e3fb97520700674d4b4bd4d8ec80d44
Ben Allen <benalleng@gmail.com>
2022-09-06 12:19:56 -0400

    WIP: redesign user and wish databases

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { type: String, required: true},
-    password: { type: String, required: true}
+    password: { type: String, required: true},
+    isAdmin: Boolean
 }, { timestamps: true})
 
 module.exports = mongoose.model('User', userSchema);
--- models/wish.js
+++ models/wish.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const wishSchema = new Schema({
-    Thing: {type: String, required: true},
-    Type: {type: String, required: true},
-    Price: {type: Number, required: true},
-    purchased: Boolean
+    name: {type: String, required: true},
+    img: {type: String, required: true},
+    description: {type: String, required: true},
+    tags: {type: Array, required: false},
+    owner: {type: String, required: true},
+    price: {type: Number, required: true},
+    admin: {type:String, default:'admin67890'},
+    isPurchased: Boolean,
 }, { timestamps: true });
 
 module.exports = mongoose.model('Wish', wishSchema);
